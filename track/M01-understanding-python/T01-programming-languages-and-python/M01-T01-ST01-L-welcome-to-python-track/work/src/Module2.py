d360515fec2005cae947d2f7d57045a1715fe458
Nare Sreelatha <your_github_email@example.com>
2026-08-03 17:20:47 +0530

    day6changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/Module2.py
@@ -0,0 +1,8 @@
+student_name="Arya"
+student_age= 21
+student_percentage=9.1
+is_registered=True
+print(type(student_name))
+print(type(student_age))
+print(type(student_percentage))
+print(type(is_registered))
