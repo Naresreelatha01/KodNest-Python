52eb15665926160f29e2bf2ddeddb3e4bab68baf
Nare Sreelatha <your_github_email@example.com>
2026-08-04 17:11:46 +0530

    changes2

--- track/M02/To3/src/typeconversion.py
+++ track/M02/To3/src/typeconversion.py
@@ -1,13 +1,9 @@
 age_text = "21"
 course_fee_text = "499.50"
 attempts = 3
-
-# Convert the values here
 age = int(age_text)
 course_fee = float(course_fee_text)
 attempts_text = str(attempts)
-
-# Display the converted values and their data types
 print(age)
 print(type(age))
 print(course_fee)
