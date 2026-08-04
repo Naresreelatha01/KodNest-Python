276aca26ea8aaa647b44eb0cf36fee90a5806c27
Nare Sreelatha <your_github_email@example.com>
2026-08-04 17:15:57 +0530

    changes2

---
+++ track/M02/To3/src/formatted.py
@@ -0,0 +1,10 @@
+# Store the student details in the specified variable names
+student_name = input()
+student_age = int(input())
+course_rating = float(input())
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
